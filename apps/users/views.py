3c7aa121271d1b52640ebfb6b86d8bbe0c9c2fa2
shokh0126mardonov <smardonov203@gmail.com>
2026-03-01 12:09:35 +0500

    full auth

--- apps/users/views.py
+++ apps/users/views.py
@@ -58,47 +58,55 @@ r = redis.Redis(
     port=6379,
     decode_responses=True
 )
+import redis
+import re
+
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+r = redis.Redis(
+    host="127.0.0.1",
+    port=6379,
+    decode_responses=True
+)
+
 
 class TelegramLoginView(APIView):
 
     permission_classes = [AllowAny]
 
-    def post(self, request):
+    def post(self, request:Request)->Response:
+        r_code = request.data.get("r_code")
 
-        code = request.data.get("otp_code")
+        if r_code is None or len(r_code) != 6 or not r_code.isdigit():
+            return Response({"code":"error"},status=status.HTTP_400_BAD_REQUEST)
+        
+        user_id = r.get(f"login_code:{r_code}")
 
-        if not code or len(code) != 6 :
-            return Response(
-                {"error": "Noto‘g‘ri kod formati"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        redis_key = f"login_code:{code}"
-
-        tg_id = r.get(redis_key)
-
-        if not tg_id:
+        if not user_id:
             return Response(
                 {"error": "Kod xato yoki muddati o‘tgan"},
                 status=status.HTTP_400_BAD_REQUEST
             )
-
-        r.delete(redis_key)
-
+        
         try:
-            user = User.objects.get(chat_id=tg_id)
+            user = User.objects.get(chat_id = user_id)
         except User.DoesNotExist:
-            return Response(
-                {"error": "User topilmadi"},
-                status=status.HTTP_404_NOT_FOUND
-            )
+            return Response("user not found",status=400)
+        
+        r.delete(f"login_user:{user_id}")
+        r.delete(f"{f"login_code:{r_code}"}")
 
-        refresh = RefreshToken.for_user(user)
+        token = RefreshToken.for_user(user)
 
-        return Response(
-            {
-                "refresh": str(refresh),
-                "access": str(refresh.access_token),
-            },
-            status=status.HTTP_200_OK
-        )
+        return Response({
+                "refresh": str(token),
+                "access": str(token.access_token),
+            },status=200)
+
