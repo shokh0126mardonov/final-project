2cb5ff1c7ff781cdd8b97cce4722650eb5beb874
shokh0126mardonov <smardonov203@gmail.com>
2026-03-02 14:15:51 +0500

    seller-profile

--- apps/sellers/models.py
+++ apps/sellers/models.py
@@ -5,7 +5,14 @@ User = get_user_model()
 
 
 class SellerProfile (models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,verbose_name="Do'kon egasi")
     shop_name = models.CharField(unique=True,verbose_name="Do'kon nomi")
-    shop_description = models.TextField(blank=True,null=True)
-    shop_logo = models.ImageField(upload_to=None,blank=True,null=True)
+    shop_description = models.TextField(blank=True,null=True,verbose_name="Do'kon tarifi")
+    shop_logo = models.ImageField(upload_to="seller/%Y/%m/%d/",blank=True,null=True,verbose_name="Do'kon logosi")
+    region = models.CharField(verbose_name="Viloyat")
+    district = models.CharField(verbose_name="Tuman")
+    address = models.CharField(blank=True,verbose_name="Manzili")
+    rating = models.FloatField(default=0,verbose_name="O'rtacha reyting")
+    total_sales = models.PositiveIntegerField(default=0,verbose_name="Sotuvlar soni")
+    created_at = models.DateTimeField(auto_now_add=False,verbose_name="yaratilgan vaqti")
+    updated_at = models.DateTimeField(auto_now=False,verbose_name="yangilangan vaqti")
