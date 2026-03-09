9045ab42ddcb8087c70d060328cab837fa2d6504
shokh0126mardonov <smardonov203@gmail.com>
2026-03-09 22:30:04 +0500

    refactore

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', default=False, cast=bool)
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
