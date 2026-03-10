a57a5d085920f8f0e479febf34de666b7d3f908b
shokh0126mardonov <smardonov203@gmail.com>
2026-03-10 12:35:00 +0500

    done

--- core/settings.py
+++ core/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
-
+ALLOWED_HOSTS = [config("ALLOWED_HOSTS")]
 
 # Application definition
 
